b87b550509f40d861cfde4f42ee7da9dfc49326a
sergivb01 <sergivb01@gmail.com>
2019-03-10 17:09:53 +0100

    Added GetBans, GetServers and GetGameTime

--- examples/main.go
+++ examples/main.go
@@ -8,11 +8,29 @@ import (
 
 func main() {
 	cli := truckersmp.NewClient(nil)
-	p, err := cli.PlayerLookup("76561198057763917")
+	p, err := cli.GetPlayer("76561198057763917")
 	if err != nil {
 		fmt.Println(err)
 	}
-
 	fmt.Println(p)
+
+	// the example player has no bans - it will return an empty slice of bans
+	bans, err := cli.GetPlayerBans("76561198057763917")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(bans)
+
+	servers, err := cli.GetServers()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(servers)
+
+	time, err := cli.GetGameTime()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%d\n", time)
 
 }
